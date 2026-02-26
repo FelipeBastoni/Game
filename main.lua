94ca208e26c139e9f4fd5faf92d0e9f9e8819db9
Felipe Bertão Bastoni <163520381+FelipeBastoni@users.noreply.github.com>
2026-02-26 13:46:05 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,8 +3,11 @@
 
 function love.load()
 
+    p_up = love.graphics.newImage("prime2.png") 
     p_right = love.graphics.newImage("player.jpg")
     p_left = love.graphics.newImage("player2.png")
+
+    grama = love.graphics.newImage("grama.png")
 
     player = {}
     player.x = 300
@@ -13,7 +16,7 @@ function love.load()
     player.sprite = p_right
 
 
-    love.window.setMode(1800, 900)
+    love.window.setMode(1920, 1080)
 
 end
 
@@ -21,6 +24,7 @@ function love.update(dt)
 
     if love.keyboard.isDown("w") then
         player.y = player.y - player.speed * dt
+        player.sprite = p_up
 
     end
 
@@ -46,7 +50,42 @@ end
 
 
 function love.draw()
+
+    local screenWidth = love.graphics.getWidth()
+    local screenHeight = love.graphics.getHeight()
+
+    love.graphics.push()
+    love.graphics.translate(
+
+        -player.x + screenWidth/ 2,
+        -player.y + screenHeight / 2
+
+    )
+
+
+
+    for x = 0, 1910, 191 do
+
+        for y = 0, 1146, 191 do 
+        
+            love.graphics.draw(grama, x, y)
+
+        end
+
+    end
+
+
+
+
+
+
+
+
+
+
     love.graphics.draw(player.sprite, player.x, player.y)
+
+    love.graphics.pop()
 
 
 end
