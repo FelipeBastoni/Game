3bb1317fadfb89518ca08c4af5e40ac8be3ef9a4
FelipeBastoni <felipebertaobastoni@gmail.com>
2026-02-28 19:00:38 -0300

    update

--- main.lua
+++ main.lua
@@ -3,50 +3,130 @@
 
 function love.load()
 
-    p_up = love.graphics.newImage("primeofc.png") 
-    p_right = love.graphics.newImage("primestp1.png")
-    p_left = love.graphics.newImage("primestp2.png")
+    p_default = {
+        love.graphics.newImage("primeofc.png"), 
+        love.graphics.newImage("primeofcesq.png") 
+
+    }
+
+    p_right = {
+        love.graphics.newImage("primestp1.png"),
+        love.graphics.newImage("primestp2.png")
+
+    }
+
+    p_left = {
+        love.graphics.newImage("primestp1esq.png"),
+        love.graphics.newImage("primestp2esq.png")
+    
+    }
+
+
+
 
     grama = love.graphics.newImage("grama.png")
 
     player = {}
     player.x = 300
     player.y = 200
-    player.speed = 200
-    player.sprite = p_up
+    player.speed = 500
+    player.sprite = p_default[1]
 
 
     love.window.setMode(1920, 1080)
 
 end
 
+
+local step = 0
+local position = ""
+
 function love.update(dt)
 
-    if love.keyboard.isDown("w") then
-        player.y = player.y - player.speed * dt
-        player.sprite = p_up
+    step = step + dt
+
+    if position == "E" then
+
+        player.sprite = p_default[2]
 
     end
 
+    if position == "D" then
+
+        player.sprite = p_default[1]
+
+    end
+
+
+
+
+
+
+    if love.keyboard.isDown("w") then
+        
+        player.y = player.y - player.speed * dt
+
+    end
+
+
+
+
+
     if love.keyboard.isDown("s") then
+     
         player.y = player.y + player.speed * dt
 
     end
 
+
+
+
+
+
+
+
     if love.keyboard.isDown("a") then
+ 
         player.x = player.x - player.speed * dt
-        player.sprite = p_left
+        player.sprite = p_left[1]
+
+
+        if step > 0.25 then
+            player.sprite = p_left[2]
+
+            if step > 0.5 then
+                step = 0
+            end
+        end
+
+        position = "E"
 
     end
 
+
+
+
     if love.keyboard.isDown("d") then
         player.x = player.x + player.speed * dt
-        player.sprite = p_right
+        player.sprite = p_right[1]
             
+        if step > 0.25 then
+            player.sprite = p_right[2]
+
+            if step > 0.5 then
+                step = 0
+            end
+        end
+
+        position = "D"
+
     end
 
 
 end
+
+
+
 
 
 function love.draw()
