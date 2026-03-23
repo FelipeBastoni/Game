280121a750b56936621d2ece916bb76c6421dc32
Felipe Bertão Bastoni <felipebertaobastoni@gmail.com>
2026-03-22 21:12:20 -0300

    Update main.lua

--- Client/main.lua
+++ Client/main.lua
@@ -4,8 +4,7 @@
 local enet = require("enet")
 
 local host = enet.host_create()
-local server = host:connect("127.0.1.0:6789")
-
+local server = host:connect("127.0.0.1:6789")
 
 
 
@@ -13,9 +12,9 @@ local server = host:connect("127.0.1.0:6789")
 
 --Messager (envio de dados (cliente --> Servidor))
 
-function messager(a, b, c, d)
+function messager(sentido, x, y, vida, id)
 
-        local msg = ""..a..";"..b..";"..c..";"..d..";"
+        local msg = ""..sentido..";"..x..";"..y..";"..vida..";"..id..";"
 
         server:send(msg)
 
@@ -110,6 +109,7 @@ function love.load()
     player.x = 300
     player.y = 200
     player.speed = 450  
+    player.position = "D"
     player.sprite = p_default[1]
 
 
@@ -117,14 +117,19 @@ function love.load()
 
     --inimigo
 
+    id = 0
+
     inimigo = {}
-    inimigo.x = 200
-    inimigo.y = 200
-    inimigo.speed = 450  
-    inimigo.sprite = p_default[3]
 
+    inimigo[id] = {}
+    inimigo[id].x = 200
+    inimigo[id].y = 200
+    inimigo[id].speed = 450
+    inimigo[id].position = ""
+    inimigo[id].sprite = p_default[3]
+    inimigo[id].id = id
 
-
+    jogadores = {}
 
 
 
@@ -156,6 +161,7 @@ function love.update(dt)
     step = step + dt
 
 
+
 --Quando temos resposta do servidor
 
     if event then
@@ -170,6 +176,7 @@ function love.update(dt)
     --Onde a mágica acontece
 
         elseif event.type == "receive" then
+
             print("Servidor respondeu:", event.data)
 
 
@@ -179,25 +186,90 @@ function love.update(dt)
 
 
 
+
         --Quando Resposta:
 
 
 
-            if t[1] == "en" then
+
+            if t[1] == "log" then
 
                 en = true
+            
+                id = tonumber(t[5])
+
+                inimigo[id] = {}
+
+                inimigo[id].x = 0
+                inimigo[id].y = 0
+                inimigo[id].speed = 0
+                inimigo[id].position = ""
+                inimigo[id].sprite = p_default[3]
+                inimigo[id].id = id
+
+                print("novo "..id)
 
             end
 
 
+            if t[1] == "logp" then
+
+                idp = tonumber(t[5])
+
+                inimigo[idp] = {}
+
+                inimigo[idp].x = 0
+                inimigo[idp].y = 0
+                inimigo[idp].speed = 0
+                inimigo[idp].position = ""
+                inimigo[idp].sprite = p_default[3]
+                inimigo[idp].id = idp
+
+                table.insert(jogadores, idp)
+
+                print("Carregado: "..idp)
 
 
+            end
 
 
+            if t[1] == "newp" then
+
+                idp = tonumber(t[5])
+
+                inimigo[idp] = {}
+
+                inimigo[idp].x = 0
+                inimigo[idp].y = 0
+                inimigo[idp].speed = 0
+                inimigo[idp].position = ""
+                inimigo[idp].sprite = p_default[3]
+                inimigo[idp].id = idp
+
+                table.insert(jogadores, idp)
+
+                print("Carregado: "..idp)
 
 
+            end
 
 
+            if t[1] == "loadp" then
+
+                idp = tonumber(t[5])
+
+                inimigo[idp].x = t[2]
+                inimigo[idp].y = t[3]
+                inimigo[idp].speed = t[4]
+                inimigo[idp].position = ""
+                inimigo[idp].sprite = p_default[3]
+                inimigo[idp].id = idp
+
+
+                print("Load: "..idp)
+
+
+            end
 
 
 
@@ -218,27 +290,38 @@ function love.update(dt)
 
  --Posição do Personagem:
 
-    if position == "E" then
+
+    position = "P"
+
+
+    if player.position == "E" and position == "P" then
 
         player.sprite = p_default[2]
+        player.position = "PE"
+
 
     end
 
-    if position == "D" then
+    if player.position == "D" and position == "P" then
 
         player.sprite = p_default[1]
+        player.position = "PD"
+
 
     end
 
-    if position == "U" then
+    if player.position == "U" and position == "P" then
 
         player.sprite = p_default[4]
+        player.position = "PU"
+
 
     end
 
-    if position == "S" then
+    if player.position == "S" and position == "P" then
 
         player.sprite = p_default[3]
+        player.position = "PS"
 
     end
 
@@ -264,7 +347,8 @@ function love.update(dt)
             end
         end
 
-        position = "U"
+        player.position = "U"
+        position = "M"
 
     end
 
@@ -286,7 +370,8 @@ function love.update(dt)
             end
         end
 
-        position = "S"
+        player.position = "S"
+        position = "M"
 
     end
 
@@ -311,7 +396,8 @@ function love.update(dt)
             end
         end
 
-        position = "E"
+        player.position = "E"
+        position = "M"
 
     end
 
@@ -321,7 +407,7 @@ function love.update(dt)
     if love.keyboard.isDown("d") then
         player.x = player.x + player.speed * dt
         player.sprite = p_right[1]
-            
+
 
         if step > 0.25 then
             player.sprite = p_right[2]
@@ -331,121 +417,19 @@ function love.update(dt)
             end
         end
 
-        position = "D"
+        player.position = "D"
+        position = "M"
 
     end
+
 
 
 
     --Envia posição ao Server
 
-    if step >= 0.041 then
+    if step >= 0.041 and id ~= 0 then
 
-        messager(position, player.x, player.y, player.speed)
-
-    end
-
-
-
-    --Informação Inimigo
-
-
-    if en == true then
-
-        inimigo.x = t[2]
-        inimigo.y = t[3]
-        
-        if t[4] == "U" then
-
-            inimigo.sprite = p_up[1]
-
-            if step > 0.25 then
-                inimigo.sprite = p_up[2]
-
-                if step > 0.5 then
-                    step = 0
-                end
-
-            end
-
-        end
-
-
-        if t[4] == "S" then
-
-            inimigo.sprite = p_down[1]
-
-            if step > 0.25 then
-                inimigo.sprite = p_down[2]
-            
-                if step > 0.5 then
-                    step = 0
-                end
-
-            end
-
-        end
-
-
-        if t[4] == "E" then
-
-            inimigo.sprite = p_left[1]
-
-            if step > 0.25 then
-                inimigo.sprite = p_left[2]
-
-                if step > 0.5 then
-                    step = 0
-                end
-
-            end
-        
-        end
-
-
-        if t[4] == "D" then
-
-            inimigo.sprite = p_right[1]
-
-            if step > 0.25 then
-                inimigo.sprite = p_right[2]
-
-                if step > 0.5 then
-                    step = 0
-                end
-            
-            end
-
-        end
-
-
-        if t[4] == "PU" then
-
-            inimigo.sprite = p_default[4]
-
-        end
-
-
-        if t[4] == "PS" then
-
-            inimigo.sprite = p_default[3]
-
-        end
-
-
-        if t[4] == "PE" then
-
-            inimigo.sprite = p_default[2]
-
-        end
-
-
-        if t[4] == "PD" then
-
-            inimigo.sprite = p_default[1]
-
-        end
-
+        messager(player.position, player.x, player.y, player.speed, id)
 
     end
 
@@ -497,10 +481,14 @@ function love.draw()
 
 
 
- --Gerador de Inimigo
+    for n=1, #jogadores, 1 do
 
-    love.graphics.draw(inimigo.sprite, inimigo.x, inimigo.y)
+        infa = jogadores[n]
 
+        love.graphics.draw(inimigo[infa].sprite, inimigo[infa].x, inimigo[infa].y)
+
+
+    end
 
 
 
