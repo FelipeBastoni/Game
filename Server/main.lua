e12afbedc88b54cd791a886e2d860a55d850342c
Felipe Bertão Bastoni <felipebertaobastoni@gmail.com>
2026-03-22 21:13:14 -0300

    Update main.lua

--- Server/main.lua
+++ Server/main.lua
@@ -11,13 +11,63 @@ local host = enet.host_create("*:6789")
 
 --Messager (envios de dados Servidor --> Cliente)
 
-function messager(peer, sentido, x, y, speed)
+function messager(peer, sentido, x, y, speed, id)
 
-    local msg = ""..sentido..";"..x..";"..y..";"..speed..";"
+    local msg = ""..sentido..";"..x..";"..y..";"..speed..";"..id..";"
 
     peer:send(msg)
 
 end
+
+
+
+
+-- Envio de dados Servidor --> Clientes
+
+function messager_all(sentido, x, y, speed, id)
+
+    local msg = ""..sentido..";"..x..";"..y..";"..speed..";"..id..";"
+
+    for n=1, np, 1 do
+
+        peer = ids[n]
+
+        peer:send(msg)
+
+    end
+    
+end
+
+
+
+
+-- Envio de dados Cliente --> Servidor --> Clientes
+
+function messager_all_minus(fonte, sentido, x, y, speed, id)
+
+    local msg = ""..sentido..";"..x..";"..y..";"..speed..";"..id..";"
+
+    print("\n")
+
+    print("fonte "..tostring(fonte))
+
+
+    for n=1, np, 1 do
+
+        peer = ids[n]
+
+        if peer ~= fonte then 
+
+            print("mandou para "..tostring(peer))
+
+            peer:send(msg)
+
+        end
+
+    end
+
+end
+
 
 
 
@@ -46,18 +96,23 @@ end
 
 function love.load()
 
-en = {}
-en.x = 0
-en.y = 0
-en.s = ""
+    np = 0
 
-player1 = {}
-player1.con = ""
-player1.x = 0
-player1.y = 0
-player1.hp = 0
+    en = {}
+    en.x = 0
+    en.y = 0
+    en.s = 0
+    en.id = 0
 
 
+    id = 0
+
+    ids = {}
+
+    en[np] = {}
+    en[np].x = 0
+    en[np].y = 0
+    en[np].s = 0
 
 
 end
@@ -96,17 +151,57 @@ function love.update(dt)
 
             if event.type == "connect" then
                     
-                print("conectado")
+                print("conectado", event.peer)
 
+            --incrementa id
+
+                np = np + 1
+
+            --notifica cliente
+                messager(event.peer, "log", 0, 0, 0, np)                
+
+
+
+            --inclui objeto no servidor
+
+                en[np] = {}
+
+                en[np].x = 0
+                en[np].y = 0
+                en[np].s = 0
+                en[np].id = np
+
+
+            --inclui ip na lista
+
+                table.insert(ids, event.peer)
+        
+
+                for n=1, (np-1), 1 do
+
+                    messager(event.peer, "logp", 0, 0, 0, n)
+                
+                end
+                
+
+
+            -- novo jogador
+
+                messager_all_minus(event.peer, "newp", 0, 0, 0, np)
+            
 
      --Se recebeu resposta
             
             elseif event.type == "receive" then
-                print(event.data)
+               -- print(event.data)
             
                 --Extrai os dados
                 t = {}
                 update(event.data)
+
+                id = tonumber(t[5])
+
+                messager_all_minus(event.peer, "loadp", t[2], t[3], t[1], id)
 
 
             end
@@ -115,45 +210,34 @@ function love.update(dt)
 
 
 
+
+
+
+
      --Calcula as Movimentações
 
-
-        if t[1] == en.s and en.s == "U" and t[2] == en.x and t[3] == en.y then
-            messager(event.peer,"en", en.x,en.y,"PU")
-
-
-        elseif t[1] == en.s and en.s == "S" and t[2] == en.x and t[3] == en.y then
-            messager(event.peer,"en", en.x,en.y,"PS")
-
-
-        elseif t[1] == en.s and en.s == "E" and t[2] == en.x and t[3] == en.y then
-            messager(event.peer,"en", en.x,en.y,"PE")
-
-
-        elseif t[1] == en.s and en.s == "D" and t[2] == en.x and t[3] == en.y then
-            messager(event.peer,"en", en.x,en.y,"PD")
-
-       
-
+        --define movimento
             
-        elseif t[1] == "U" then 
+        --com id
 
-            en.x = t[2]
-            en.y = t[3]
-            en.s = t[1]
 
-            messager(event.peer,"en", en.x,en.y,en.s)
+        if t[1] == "U" then 
+
+            en[id].x = t[2]
+            en[id].y = t[3]
+            en[id].s = t[1]
+
 
        
 
         elseif t[1] == "S" then 
 
-            en.x = t[2]
-            en.y = t[3]
-            en.s = t[1]
+            en[id].x = t[2]
+            en[id].y = t[3]
+            en[id].s = t[1]
 
 
-            messager(event.peer,"en", en.x,en.y,en.s)
+
 
         
 
@@ -161,11 +245,11 @@ function love.update(dt)
 
         elseif t[1] == "E" then 
 
-            en.x = t[2]
-            en.y = t[3]
-            en.s = t[1]
+            en[id].x = t[2]
+            en[id].y = t[3]
+            en[id].s = t[1]
 
-            messager(event.peer,"en", en.x,en.y,en.s)
+
 
       
 
@@ -173,18 +257,66 @@ function love.update(dt)
 
         elseif t[1] == "D" then 
 
-            en.x = t[2]
-            en.y = t[3]
-            en.s = t[1]
+            en[id].x = t[2]
+            en[id].y = t[3]
+            en[id].s = t[1]
 
-            messager(event.peer,"en", en.x,en.y,en.s)
+
+
+
+
+
+
+        elseif t[1] == "PU" then 
+
+            en[id].x = t[2]
+            en[id].y = t[3]
+            en[id].s = t[1]
+
+
+
+
+
+
+
+        elseif t[1] == "PS" then 
+
+            en[id].x = t[2]
+            en[id].y = t[3]
+            en[id].s = t[1]
+
+
+
+
+
+
+
+        elseif t[1] == "PE" then 
+
+            en[id].x = t[2]
+            en[id].y = t[3]
+            en[id].s = t[1]
+
+
+
+
+
+
+
+        elseif t[1] == "PD" then 
+
+            en[id].x = t[2]
+            en[id].y = t[3]
+            en[id].s = t[1]
+
+
+
 
         end
 
 
-
-
     end
+
 
 
 
