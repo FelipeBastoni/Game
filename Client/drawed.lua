f5f350ad6a01c418aa8cc162e8fc6ee2576bd360
Felipe Bertão Bastoni <felipebertaobastoni@gmail.com>
2026-05-11 22:11:32 -0300

    Update drawed.lua

--- Client/drawed.lua
+++ Client/drawed.lua
@@ -9,22 +9,24 @@ local drawed = {}
 function drawed.draw(grama, mapa, v_tiles, h_tiles, tile_width, tile_height, left_corner)
 
     for i = 1, v_tiles, 1 do
-        for j = left_corner, h_tiles, 1 do
+        for j = 1, h_tiles, 1 do
             if (mapa[i][j] == "T") then
 
 
                 table.insert(co_mun, {
 
-                x = (j-left_corner)*tile_height,
-                y = (i-1)*tile_width})
+                x = j*tile_width,
+                y = i*tile_height,
+                w = 192,
+                h = 192})
 
 
 
-                love.graphics.draw(grama, ((j-left_corner)*tile_height), ((i-1)*tile_width))
+                love.graphics.draw(coli, (j*tile_height), (i*tile_width))
             elseif (mapa[i][j] == "G") then
-                love.graphics.draw(grama, ((j-left_corner)*tile_height), ((i-1)*tile_width))
+                love.graphics.draw(grama, (j*tile_height), (i*tile_width))
             elseif (mapa[i][j] == "P") then
-                love.graphics.draw(stone, ((j-left_corner)*tile_height), ((i-1)*tile_width))
+                love.graphics.draw(stone, (j*tile_height), (i*tile_width))
             end
         end
     end
